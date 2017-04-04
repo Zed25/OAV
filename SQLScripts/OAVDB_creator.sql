42b1db5362bab81438dafb07e9758372422a6c97
dilettalagom <dilettalagom@gmail.com>
2017-04-04 18:28:46 +0200

    DB creators

--- SQLScripts/OAVDB_creator.sql
+++ SQLScripts/OAVDB_creator.sql
@@ -1,91 +1,91 @@
-create table "Agenzie"(
-	"Nome" character varying(10) primary key
+create table Agencies (
+	Name character varying (10) primary key
 );
 
-create table "Satelliti"(
-	"Nome" character varying(10) primary key,
-	"DataInizioMissione" timestamp not null,
-	"DataFineMissione" timestamp 
+create table Satellites (
+	Name character varying (10) primary key,
+	StartMissionDate timestamp not null,
+	EndMissionDate timestamp
 );
 
-create table "Partecipazione_Missione"(
-	"Agenzia" character varying(10) references "Agenzie"("Nome"),
-	"Satellite" character varying(10) references "Satelliti"("Nome"),
-	 primary key ("Agenzia", "Satellite")
+create table MissionsJoined(
+	Agency character varying (10) references Agencies (Name),
+	Satellite character varying (10) references Satellites (Name),
+	primary key (Agency, Satellite)
 );
 
-create table "Mappe_Stellari"(
-	"Nome" character varying(10) primary key
+create table  StarMaps(
+	Name character varying (10) primary key
 );
 
-create table "Strumenti"(
-	"Nome" character varying(10) primary key,
-	"Satellite" character varying(10) references "Satelliti"("Nome"),
-	"MappaStellare" character varying(10) references "Mappe_Stellari"("Nome")
+create table Instruments (
+	Name character varying (10) primary key,
+	Satallite character varying (10) references Satellites (Name),
+	StarMap character varying (10) references StarMaps (Name)
 );
 
-create table "Bande"(
-	"IDBanda" integer primary key,
-	"Risoluzione" real not null,
-	"LunghezzaSpettrale" real not null
+create table Bands(
+	BandID integer primary key,
+	Resolution real not null,
+	SpectralLength real not null
 );
 
-create table "Rilevazione_S-B"(
-	"Strumento" character varying(10) references "Strumenti"("Nome"),
-	"Banda" integer references "Bande"("IDBanda"),
-	primary key ("Strumento", "Banda")
+create table S-BSurbay(
+	Instrument character varying (10) references Instruments (Name),
+	Band integer references Bands (BandID),
+	primary key (Instrument, Band)
 );
 	
-create table "Sorgenti"(
-	"IDSorgente" character varying(20) primary key,
-	"Luminosità" real not null,
-	"Tipo" character varying(10) not null
+create table Sources (
+	SourceID character varying (20) primary key,
+	Luminosity real not null,
+	Type character varying (10) not null
 );
 
-create table "Afferenza_M_S-S"(
-	"MappaStellare" character varying(10) references "Mappe_Stellari"("Nome"),
-	"Sorgente" character varying(20) references "Sorgenti"("IDSorgente"),
-	primary key ("MappaStellare","Sorgente")
+create table Collection (
+	StarMap character varying (10) references StarMaps(Name),
+	Source character varying (20) references Sources(SourceID),
+	primary key (StarMap, Source)
 );
 
-create table "Confronto_S-S"( 
-	"SorgenteConfrontante" character varying(20) references "Sorgenti"("IDSorgente"),
-	"SorgenteConfrontata" character varying(20) references "Sorgenti"("IDSorgente"),
-	primary key ("SorgenteConfrontante","SorgenteConfrontata")
+create table S_S_Comparison(
+	ComparingSource character varying (20) references Sources(SourceID),
+	ComparedSource character varying (20) references Sources(SourceID),
+	primary key (ComparingSource, ComparedSource)
 );
 
-create table "Clumps"(
-	"IDClump" integer primary key,
-	"LongitudineGalattica" double precision not null,
-	"LatitudineGalattica" double precision not null,
-	"Temperatura" real not null,
-	"RapportoTBM" real not null,
-	"Massa" real not null,
-	"DensitàSuperficie" real not null,
-	"Tipo"  decimal not null,
-	"MappaHigal" character varying(10) references "Mappe_Stellari"("Nome")
+create table Clumps(
+	ClumpID integer primary key,
+	GalacticLongitude double precision not null,
+	GalacticLatitude double precision not null,
+	Temperature real not null,
+	LMRatio real not null,
+	Mass real not null,
+	SurfeceDensity real not null,
+	Type decimal not null,
+	HigalMaps character varying (10) references StarMaps (Name)
 );
 
-create table "Appartentenza_S-C"(
-	"Sorgente" character varying(20) references "Sorgenti"("IDSorgente"),
-	"Clump" integer references "Clumps"("IDClump"),
-	primary key ("Sorgente","Clump")
+create table S-C_Membership(
+	Source character varying (20) references Sources (SourceID),
+	Clump integer references Clumps (ClumpID),
+	primary key (Source,Clump)
 );
 
-create table "Ellisse"(
-	"Clump" integer references "Clumps"("IDClump"),
-	"Banda" integer references "Bande"("IDBanda"),
-	"AsseX" double precision not null,
-	"AsseY" double precision not null,
-	"AngoloRotazione" double precision not null,
-	primary key ("Clump","Banda")
+create table Ellipses (
+	Clump integer references Clumps(ClumpID),
+	Band integer references Bands(BandID),
+	Xaxis double precision not null,
+	Yaxis double precision not null,
+	PositionAngle double precision not null,
+	primary key (Clump , Band)
 );
 
-create table "Flussi"(
-	"IDFlusso" integer primary key,
-	"Valore" real not null,
-	"Errore" real,
-	"Banda" integer references "Bande"("IDBanda"),
-	"Sorgente" character varying(20) references "Sorgenti"("IDSorgente"),
-	"Clump" integer references "Clumps"("IDClump")
+create table Fluxes (
+	FluxID integer primary key,
+	Value real not null,
+	Error real,
+	Band integer references Bands (BandID),
+	Source character varying (20) references Sources (SourceID),
+	Clump integer references Clumps (ClumpID)
 );
--- SQLScripts/UserTable_creator.sql
+++ SQLScripts/UserTable_creator.sql
@@ -1,8 +1,8 @@
-create table "Utenti"(
-	"User-id" character varying(20) primary key,
-	"Password" character(32) not null,
-	"Nome" character varying(20) not null,
-	"Cognome" character varying(20) not null,
-	"email" text not null,
-	"tipo" character varying(20) not null
+create table Users (
+	IDUser character varying(20) primary key,
+	Password character(32) not null,
+	Name character varying(20) not null,
+	Surname character varying(20) not null,
+	Email text not null,
+	Type character varying(20) not null
 );
